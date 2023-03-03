95642f9b16c14cbca032604979b3eaa724aa398b
Junior Prado <juniorcvnnn@gmail.com>
2023-03-03 19:42:07 -0300

    mod 1

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv" 
 
+dotenv.config()
 export const app = express();
 
 app.use(express.json());
 app.use(cors());
+const port=process.env.PORT ||3002
 
-app.listen(3003,() => {
-    console.log("Servidor rodando na porta 3003");
-    
+app.listen(port,() => {
+    console.log(`Servidor rodando na porta ${port}`);
 })
